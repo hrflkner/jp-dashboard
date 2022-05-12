094cf59fb2e5176212b099e174128a0cdd2e6b62
hrflkner <hunter.wm.faulkner@gmail.com>
2022-05-12 21:44:22 +1000

    Delete Data File

--- src/components/ui/Main/Main.jsx
+++ src/components/ui/Main/Main.jsx
@@ -3,15 +3,17 @@ import ThemeContext from '../../context/ThemeContext';
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
-    background-color: ${({ theme }) => theme.bodyBackgroundColor};
+    // Layout
     width: 100vw;
     height: 100vh;
     padding: 0.5rem;
     padding-left: 25rem;
+
+    // Themed Styling
+    background-color: ${({ theme }) => theme.bodyBackgroundColor};
 `;
 function Main({ children }) {
     const { theme } = useContext(ThemeContext);
-
     return <StyledMain theme={theme.themeData}>{children}</StyledMain>;
 }
 
